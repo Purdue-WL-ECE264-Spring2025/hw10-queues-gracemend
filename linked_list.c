51452ad495c7f62bd51b9c087546baad39bac445
gracemend <165606222+gracemend@users.noreply.github.com>
2025-04-04 00:13:35 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -4,15 +4,49 @@
 
 struct list_node *new_node(size_t value) { return NULL; }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node * node = new_node(value);
+  node->next = list->head;
+  list->head = node;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node * node = new_node(value);
+  struct list_node* curr = list->head;
+  while(curr->next != NULL){
+    curr = curr->next;
+  }
+  curr->next = node;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  struct list_node* del = list->head;
+  list->head = list->head->next;
+  free(del);
+  return 0; 
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) { 
+  struct list_node* curr = list->head->next;
+  struct list_node* last = list->head;
+  while(curr->next != NULL){
+    last = curr;
+    curr = curr->next;
+  }
+  last->next = NULL;
+  free(curr);
+  return 0; 
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+  struct list_node *curr = list.head;
+  while (curr != NULL) {
+    struct list_node *del = curr;
+    curr = curr->next;
+    free(del);
+  }
+  free(list.head);
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
